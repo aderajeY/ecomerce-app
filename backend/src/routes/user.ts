b7e68d24214bc00a5e9bcc570e17a522c09e2916
Aderajew Yeshiwendm <aderajewy26@gmail.com>
2024-02-25 16:32:43 +0300

    login added

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,22 +1,42 @@
 import {Router, Response, Request} from 'express';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 const router = Router();
-import { user } from '../models/userModel';
+import { user,User } from '../models/userModel';
 import { UserErrors } from './errors';
 router.post('/signup', async (req:Request, res:Response)=>{
     const {username, password} = req.body;
     try{
-    const user1 = await user.findOne({username})
-    if (user1){
-        return res.status(400).json({type:UserErrors.exists})
+        const user1 = await user.findOne({username})
+        if (user1){
+            return res.status(400).json({type:UserErrors.exists})
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await user.create({username,password:hashedPassword});
+        await newUser.save();
+        res.json({message:"success",newUser});
+    }catch(error){
+        return res.status(500).json({message:error})
     }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await user.create({username,password:hashedPassword});
-    await newUser.save();
-    res.json({message:"success",newUser});
-}catch(error){
-    return res.status(500).json({message:error})
-}
+
+})
+router.post('/login', async (req:Request, res:Response)=>{
+    const {username, password} = req.body;
+    try{
+        const user1:User = await user.findOne({username})
+        if (!user1){
+            return res.status(400).json({type:UserErrors.user_not_found})
+        }
+        const isPasswordValid = await bcrypt.compare(password, user1.password);
+        if (!isPasswordValid){
+            return res.status(400).json({type:UserErrors.wrong_credentials})
+        }
+        const token = jwt.sign({id:user1.userId}, "secret");
+        return res.json({token, userId:user1.userId})
+        
+    }catch(error){
+        return res.status(500).json({message:error})
+    }
 
 })
 export {router as UserRouter}
