c195881017be2204bc4a08d3621ea6270d57733c
Aderajew Yeshiwendm <aderajewy26@gmail.com>
2024-03-03 14:35:39 +0300

    verify token

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # ecomerce-app
-to start navigate to the backend in the terminal and run 
 
-
-yarn add typescript cors express mongoose tsc-node bcrypt @types/express @types/node jsonwebtoken nodemon
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,42 +1,54 @@
-import {Router, Response, Request} from 'express';
+import { Router, Response, Request, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 const router = Router();
-import { user,User } from '../models/userModel';
+import { user, User } from '../models/userModel';
 import { UserErrors } from './errors';
-router.post('/signup', async (req:Request, res:Response)=>{
-    const {username, password} = req.body;
-    try{
-        const user1 = await user.findOne({username})
-        if (user1){
-            return res.status(400).json({type:UserErrors.exists})
+router.post('/signup', async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    try {
+        const user1 = await user.findOne({ username })
+        if (user1) {
+            return res.status(400).json({ type: UserErrors.exists })
         }
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await user.create({username,password:hashedPassword});
+        const newUser = await user.create({ username, password: hashedPassword });
         await newUser.save();
-        res.json({message:"success",newUser});
-    }catch(error){
-        return res.status(500).json({message:error})
+        res.json({ message: "success", newUser });
+    } catch (error) {
+        return res.status(500).json({ message: error })
     }
 
 })
-router.post('/login', async (req:Request, res:Response)=>{
-    const {username, password} = req.body;
-    try{
-        const user1:User = await user.findOne({username})
-        if (!user1){
-            return res.status(400).json({type:UserErrors.user_not_found})
+router.post('/login', async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    try {
+        const user1: User = await user.findOne({ username })
+        if (!user1) {
+            return res.status(400).json({ type: UserErrors.user_not_found })
         }
         const isPasswordValid = await bcrypt.compare(password, user1.password);
-        if (!isPasswordValid){
-            return res.status(400).json({type:UserErrors.wrong_credentials})
+        if (!isPasswordValid) {
+            return res.status(400).json({ type: UserErrors.wrong_credentials })
         }
-        const token = jwt.sign({id:user1.userId}, "secret");
-        return res.json({token, userId:user1.userId})
-        
-    }catch(error){
-        return res.status(500).json({message:error})
+        const token = jwt.sign({ id: user1.userId }, "secret");
+        return res.json({ token, userId: user1.userId })
+
+    } catch (error) {
+        return res.status(500).json({ message: error })
     }
 
 })
-export {router as UserRouter}
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        jwt.verify(authHeader, 'secret', (err) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            next();
+        })
+    }
+    return res.sendStatus(401);
+}
+export { router as UserRouter }
